1b55604e74f90edeaeee78c213fe4e91e62e471c
olehinrichs118 <120103837+olehinrichs118@users.noreply.github.com>
2024-10-22 22:56:26 +0200

    Update run.py
    API_HASH

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ from decouple import config
 APP_ID = config("APP_ID", default=0, cast=int)
 API_HASH = config("API_HASH", default=None, cast=str)
 
-with TelegramClient(StringSession(), api_id, api_hash) as client:
+with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
     print(client.session.save())
