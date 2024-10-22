5f12c201e6c70389969d5729ce51b0ccc61ddbaf
olehinrichs118 <120103837+olehinrichs118@users.noreply.github.com>
2024-10-22 22:48:38 +0200

    Update run.py
    from decouple import config

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
+from decouple import config
+
+APP_ID = config("APP_ID", default=0, cast=int)
+API_HASH = config("API_HASH", default=None, cast=str)
 
 with TelegramClient(StringSession(), api_id, api_hash) as client:
     print(client.session.save())
